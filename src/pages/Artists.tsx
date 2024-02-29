6988135e7a696172162d61dda331d3a3afd764ba
Robert Jenkins <384403+dabobert@users.noreply.github.com>
2024-02-29 15:34:22 -0500

    added artists page and removed user profile pic from header

---
+++ src/pages/Artists.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DefaultLayout from '../layout/DefaultLayout';
+
+const Files: React.FC = () => {
+  return (
+    <DefaultLayout>
+      Artists go here
+    </DefaultLayout>
+  );
+};
+
+export default Artists;
