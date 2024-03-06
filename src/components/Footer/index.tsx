a0d3025b2750effc55e5a33655ceaa27f34ade3a
Robert Jenkins <384403+dabobert@users.noreply.github.com>
2024-03-05 22:53:20 -0500

    added vidstack to app

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
-
+import { Player } from '../Player';
 const Footer: React.FC = () => {
   return (
     // <footer className="fixed inset-x-0 bottom-0 bg-gray-200 text-center py-4 text-gray-700">
     // <footer className="bg-gray-200 text-center py-4 text-gray-700">
     // <footer className="bg-gray-800 text-center p-4">
     <footer className="sticky inset-x-0 bottom-0 text-center py-4">
-      <div className="container mx-auto" style={{color: 'black'}}>
-        <h3>MP3 controls go here</h3>
-        <p className="text-xs mt-2">
-          Follow us on
-          <a href="https://twitter.com" className="text-blue-500 hover:underline ml-1">
-            Twitter
-          </a>,
-          <a href="https://facebook.com" className="text-blue-700 hover:underline ml-1">
-            Facebook
-          </a>, and
-          <a href="https://instagram.com" className="text-pink-500 hover:underline ml-1">
-            Instagram
-          </a>.
-        </p>
-      </div>
+      <Player />
     </footer>
   );
 };
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -135,14 +135,10 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
               </NavLink>
             </li>
           </ul>
-          {/* Promo/MP3 Area */}
-          {/* <div class="mx-auto mb-10 w-full max-w-60 rounded-sm border border-strokedark bg-boxdark px-4 py-6 text-center shadow-default">
-            <h3 class="mb-1 font-semibold text-white">TailAdmin Pro</h3>
-            <p class="mb-4 text-xs">Get All Dashboards and 300+ UI Elements</p>
-            <a href="https://tailadmin.com/pricing" target="_blank" rel="nofollow" class="flex items-center justify-center rounded-md bg-primary p-2 text-white hover:bg-opacity-95">
-              Purchase Now
-            </a>
-          </div> */}
+          {/* Promo/MP3/Artwork Area */}
+          <div className="mx-auto mb-10 w-full max-w-60 rounded-sm border border-strokedark bg-boxdark px-4 py-6 text-center shadow-default">
+           <img src="https://upload.wikimedia.org/wikipedia/en/3/3f/Bjork_Post.png" />
+          </div>
         </nav>
         {/* <!-- Sidebar Menu --> */}
       </div>
