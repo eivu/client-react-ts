c509ecb6dac0a1583da332be7da3cb7e79e09881
Robert Jenkins <384403+dabobert@users.noreply.github.com>
2024-03-08 14:12:59 -0500

    refactored UI - added files to left nav sidebar - fixed bug in artists component - broke out routes into its own file

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -103,6 +103,17 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
             </li>
             <li>
               <NavLink
+                to="/files"
+                className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
+                  pathname.includes('files') &&
+                  'bg-graydark dark:bg-meta-4'
+                }`}
+              >
+                Files
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
                 to="/folders"
                 className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
                   pathname.includes('folders') &&
