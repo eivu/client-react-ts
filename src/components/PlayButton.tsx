bc8ec644bc61be0508cb56446d6cbaae12cd0807
Robert Jenkins <384403+dabobert@users.noreply.github.com>
2024-03-11 14:47:43 -0400

    play button no longer triggers vidstack warnings

--- src/components/PlayButton.tsx
+++ src/components/PlayButton.tsx
@@ -10,27 +10,27 @@ import {
 
 function assignSrc(remote:MediaRemoteControl):void {
   console.log("assigning")
-  const player = remote.getPlayer();
-  console.log(remote);
-  console.log(player);
+  // const player = remote.getPlayer();
+  // console.log(remote);
+  // console.log(player);
 
 }
 
 export const PlayButton:FC = ({item}) => {
   const [hover, setHover] = useState(false);
-  const isPlaying = useMediaState('playing');
-  const remote = useMediaRemote(); // https://www.vidstack.io/docs/player/api/classes/media-remote-control?styling=default-theme
+  // const isPlaying = useMediaState('playing');
+  // const remote = useMediaRemote(); // https://www.vidstack.io/docs/player/api/classes/media-remote-control?styling=default-theme
 
-  let p = useRef<MediaPlayerInstance>(null)
-  console.log(item)
-  console.log(isPlaying)
+  // let p = useRef<MediaPlayerInstance>(null)
+  // console.log(item)
+  // console.log(isPlaying)
 
 
   return (
     <div
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
-      onClick={() => assignSrc(remote)}
+      onClick={() => assignSrc()}
     >
       {hover ? <PiPlayCircleFill size={32} className='cursor-pointer'/> : <PiPlayCircleLight size={32} className='cursor-pointer'/>}  
     </div>
