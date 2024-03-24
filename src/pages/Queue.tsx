c7fcfc5ad9bb7bddffa716a27a3ac697fb67075d
Robert Jenkins <384403+dabobert@users.noreply.github.com>
2024-03-23 20:02:25 -0400

    styling the queue page

--- src/pages/Queue.tsx
+++ src/pages/Queue.tsx
@@ -13,13 +13,19 @@ const Queue: React.FC = () => {
       <div className="flex flex-col gap-7.5">
         <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
           <div className="p-4 sm:p-6 xl:p-9">
-            <ol>
-              {
-                queue.map((item, index) => (
-                  <li>{item.name} - 0:00</li>
-                ))
-              }
-            </ol>
+            {queue.map((item, index) => (
+              <div key={index} className="grid grid-cols-12 border-b border-stroke py-3.5 pl-5 pr-6 dark:border-strokedark">
+                <div className="col-span-1">
+                  <p className="font-medium">{index + 1}</p>
+                </div>
+                <div className="col-span-10">
+                  <p className="font-medium">{item.name}</p>
+                </div>
+                <div className="col-span-1 text-right">
+                  <p className="font-medium">{item.duration}</p>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
