2de30a9706477b6b23d2ef92f6ca46846966610b
Robert Jenkins <384403+dabobert@users.noreply.github.com>
2024-03-30 23:33:40 -0400

    added MiniLoader to Laoder component

--- src/common/Loader/index.tsx
+++ src/common/Loader/index.tsx
@@ -6,4 +6,12 @@ const Loader = () => {
   );
 };
 
+export const MiniLoader = () => {
+  return (
+    <div className="flex items-center justify-center bg-white dark:bg-black">
+      <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent"></div>
+    </div>
+  );
+};
+
 export default Loader;
