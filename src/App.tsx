b8e4bd25484d4dc1ba964f1442f81265227e816d
Robert Jenkins <384403+dabobert@users.noreply.github.com>
2024-04-10 23:07:59 -0400

    feat: Implementing otp access within app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { useEffect, useState } from 'react';
+import { useCookies } from 'react-cookie';
 import Loader from './components/Loader';
 import { AppRoutes } from './AppRoutes';
 import { AppProvider } from './store/AppContext';
+import {v4 as uuidv4} from 'uuid';
+
 
 
 
 function App() {
   const [loading, setLoading] = useState<boolean>(true);
+  const [cookies, setCookie] = useCookies(['eivu']);
+uuidv4()
 
   useEffect(() => {
+    setCookie('Name', 'eivu', { path: '/' });
     setTimeout(() => setLoading(false), 1000);
   }, []);
 
