765f81d62e3c672d612a4ea64fac1b4a1b29ffa1
Robert Jenkins <384403+dabobert@users.noreply.github.com>
2024-04-18 11:33:19 -0400

    created Auth page, which can redirect to /files on click

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import axios from 'axios';
 import DefaultLayout from '../layout/DefaultLayout';
+import { useNavigate } from 'react-router-dom';
+
 
 const Auth: React.FC = () => {
+  const navigate = useNavigate();
+  const url = import.meta.env.VITE_EIVU_SERVER_HOST + '/api/frontend/v1/auth';
+  const handleClick = () => {
+    navigate('/files', { replace: true });
+
+    // axios.get(url, {
+    //   params: constructParams(sorting),
+    //   headers: {
+    //     'Authorization': 'Bearer ' + import.meta.env.VITE_EIVU_USER_TOKEN,
+    //     'Device-Uuid': cookies.deviceUuid
+    //   }})
+    //   .then((response) => {
+    //     setQueueItems(response.data.cloudFiles);
+    //     setMeta(response.data.meta);
+    //     setLoading(false);
+    //   })
+    //   .catch((error) => {
+    //     setLoading(false);
+    //     setResponseError(error.message);
+    //   });
+
+  };
+
   return (
     <DefaultLayout>
       <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
@@ -17,12 +43,10 @@ const Auth: React.FC = () => {
               placeholder="2FA Code"
               className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 my-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
             />
-            <button className="flex justify-center rounded bg-primary p-3 font-medium text-gray hover:bg-opacity-90">
+            <button onClick={handleClick} className="flex justify-center rounded bg-primary p-3 font-medium text-gray hover:bg-opacity-90">
               Authorize
             </button>
           </div>
-          
-          
         </div>
       </div>
     </DefaultLayout>
