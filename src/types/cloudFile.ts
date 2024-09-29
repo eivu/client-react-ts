c017e680c1304d212a033db122d003f0347bf0bd
“Rob <384403+dabobert@users.noreply.github.com>
2024-09-29 10:37:26 -0400

    updated cloud file type had all of its keys as camelcase

--- src/types/cloudFile.ts
+++ src/types/cloudFile.ts
@@ -6,32 +6,32 @@ export default interface CloudFile {
   name: string | null
   asset: string | null
   md5: string | null
-  content_type: string | null
+  contentType: string | null
   filesize: number | null
   description: string | null
   rating: string | null
   nsfw: boolean | null
   secured: boolean | null
-  created_at: string | null
-  updated_at: string | null
-  info_url: string | null
+  createdAt: string | null
+  updatedAt: string | null
+  infoUrl: string | null
   duration: string | null
-  ext_id: string | null
-  data_source_id: string | null
+  extId: string | null
+  dataSourceId: string | null
   release_id: string | null
   year: number | null
-  release_pos: number | null
-  num_plays: number | null
+  releasePos: number | null
+  numPlays: number | null
   state: string | null
-  last_viewed_at: string | null
+  lastViewedAt: string | null
   deletable: boolean | null
-  date_aquired: string | null
+  dateAquired: string | null
   shared: boolean | null
-  bucket_uuid: string | null
-  bucket_name: string | null
-  user_uuid: string | null
-  folder_uuid: string | null
-  artwork_md5: string | null
+  bucketUuid: string | null
+  bucketName: string | null
+  userUuid: string | null
+  folderUuid: string | null
+  artworkMd5: string | null
   metadata: Metadatum[]
   artists: Artist[]
   releases: Release[]
