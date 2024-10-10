1810e59c7355bf32940475327ea7e1def8aa073a
Rob Jenkins <384403+dabobert@users.noreply.github.com>
2024-10-09 23:54:06 -0400

    moved the guts of Releases show into ReleaseTable compomnent

---
+++ src/components/ReleaseTable.tsx
@@ -0,0 +1,92 @@
+
+import React from 'react';
+// import { timeAgo } from '../../common/timeAgo';
+// import { useMediaState } from '@vidstack/react';
+// import { useMemo, useState, useEffect, FC } from 'react';
+import { Release } from '../types/release';
+import { Link } from 'react-router-dom';
+import prettyBytes from 'pretty-bytes';
+import convertSecondsToTimeHhMmSs from '../common/convertSecondsToTimeHhMmSs';
+import { Track } from '../types/track';
+import { MiniLoader } from './Loader';
+import { AddToQueueButton } from './AddToQueueButton';
+import { AVButton } from './AVButton';
+
+
+
+export type ReleaseTableProps = {
+  release: Release;
+};
+
+export function ReleaseTable({ release }: ReleaseTableProps): React.JSX.Element {
+  const topRowKeyClassNames   = "py-2 pr-2 font-mono font-medium leading-6 whitespace-nowrap border-slate-100 dark:border-slate-400/10";
+  const topRowvalueClassNames = "break-words py-2 pl-2 font-mono leading-6 whitespace-pre border-slate-100 dark:border-slate-400/10 text-wrap";
+  const valueClassNames = topRowvalueClassNames.concat(" border-t");
+  return (
+    <table id="release-details-table" className="w-full text-left border-collapse">
+      <tbody>
+        <tr>
+          <td></td>
+          <td></td>
+          { release.multiBundle &&
+            <td className={topRowKeyClassNames}>
+              Bundle
+            </td>
+          }
+          <td className={topRowKeyClassNames}>
+            Position
+          </td>
+          <td className={topRowKeyClassNames}>
+            Name
+          </td>
+          <td className={topRowKeyClassNames}>
+            Duration
+          </td>
+          <td className={topRowKeyClassNames}>
+            Size
+          </td>
+          <td className={topRowKeyClassNames}>
+            Rating
+          </td>
+          <td className={topRowKeyClassNames}>
+            Plays
+          </td>
+        </tr>
+        {release.tracks.map((track) => (
+          <tr key={track.id}>
+            <td>
+              {/* <AddToQueueButton track={track} /> */}
+            </td>
+            <td>
+              {/* <AVButton track={track} /> */}
+            </td>
+            { 
+              release.multiBundle &&
+                <td className={valueClassNames}>
+                  {track.bundlePos}
+                </td>
+            }
+            <td className={valueClassNames}>
+              {track.position}
+            </td>
+            <td className={valueClassNames}>
+              <Link to={`/files/${track.md5}`}>{track.label}</Link>
+            </td>
+            <td className={valueClassNames}>
+              {track.duration && convertSecondsToTimeHhMmSs(track.duration)}
+            </td>
+            <td className={valueClassNames}>
+              {prettyBytes(track.filesize)}
+            </td>
+            <td className={valueClassNames}>
+              { track.rating }
+            </td>
+            <td className={valueClassNames}>
+              { track.numPlays }
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
--- src/pages/Releases/show.tsx
+++ src/pages/Releases/show.tsx
@@ -7,24 +7,14 @@ import { Link, useLoaderData } from 'react-router-dom';
 import DefaultLayout, { ContentHeader, ContentContainer } from '../../layout/DefaultLayout';
 import  { useAppContext } from '../../store/AppContext';
 // import axios from 'axios';
-import prettyBytes from 'pretty-bytes';
-import { timeAgo } from '../../common/timeAgo';
-import { useMediaState } from '@vidstack/react';
-import { useMemo, useState, useEffect, FC } from 'react';
-import AddToQueueButton from '../../components/AddToQueueButton';
-import AVButton from '../../components/AVButton';
 import type { Release } from '../../types/release';
 import { MiniLoader } from '../../components/Loader';
-import convertSecondsToTimeHhMmSs from '../../common/convertSecondsToTimeHhMmSs';
 
+import { ReleaseTable } from '../../components/ReleaseTable';
 
 
 const ReleasePage: React.FC = () => {
-  const release:Release  = useLoaderData();
-  const topRowKeyClassNames   = "py-2 pr-2 font-mono font-medium leading-6 whitespace-nowrap border-slate-100 dark:border-slate-400/10"
-  const keyClassNames   = topRowKeyClassNames.concat(" border-t")
-  const topRowvalueClassNames = "break-words py-2 pl-2 font-mono leading-6 whitespace-pre border-slate-100 dark:border-slate-400/10 text-wrap"
-  const valueClassNames = topRowvalueClassNames.concat(" border-t")
+  const release:Release  = useLoaderData()<Release>;
 
   // const [loading, setLoading] = useState<boolean>(true);
   // const [responseError, setResponseError] = useState<String | undefined>(undefined);
@@ -47,71 +37,7 @@ const ReleasePage: React.FC = () => {
         <Link to="/releases" className="breadcrumb">Release</Link>::{release.secured ? `Release ${release.id}` : release.name}
       </ContentHeader>
       <ContentContainer>
-        <table id="release-details-table" className="w-full text-left border-collapse">
-          <tbody>
-            <tr>
-              <td></td>
-              <td></td>
-              { release.multiBundle &&
-                <td className={topRowKeyClassNames}>
-                  Bundle
-                </td>
-      }
-              <td className={topRowKeyClassNames}>
-                Position
-              </td>
-              <td className={topRowKeyClassNames}>
-                Name
-              </td>
-              <td className={topRowKeyClassNames}>
-                Duration
-              </td>
-              <td className={topRowKeyClassNames}>
-                Size
-              </td>
-              <td className={topRowKeyClassNames}>
-                Rating
-              </td>
-              <td className={topRowKeyClassNames}>
-                Plays
-              </td>
-            </tr>
-            {release.tracks.map((track) => (
-              <tr key={track.id}>
-                <td>
-                  {/* <AddToQueueButton track={track} /> */}
-                </td>
-                <td>
-                  {/* <AVButton track={track} /> */}
-                </td>
-                { 
-                  release.multiBundle &&
-                    <td className={valueClassNames}>
-                      {track.bundlePos}
-                    </td>
-                }
-                <td className={valueClassNames}>
-                  {track.position}
-                </td>
-                <td className={valueClassNames}>
-                  <Link to={`/files/${track.md5}`}>{track.label}</Link>
-                </td>
-                <td className={valueClassNames}>
-                  {convertSecondsToTimeHhMmSs(track.duration)}
-                </td>
-                <td className={valueClassNames}>
-                  {prettyBytes(track.filesize)}
-                </td>
-                <td className={valueClassNames}>
-                  { track.rating }
-                </td>
-                <td className={valueClassNames}>
-                  { track.numPlays }
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <ReleaseTable release={release} />
       </ContentContainer>
     </DefaultLayout>
   );
