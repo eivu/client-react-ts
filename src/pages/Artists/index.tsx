081c284b639526592d669091d325674678f66912
Rob Jenkins <384403+dabobert@users.noreply.github.com>
2024-10-13 19:53:43 -0400

    artist index page is now working with pagination and alpha menu

--- src/pages/Artists/index.tsx
+++ src/pages/Artists/index.tsx
@@ -1,12 +1,82 @@
 import React from 'react';
 import DefaultLayout from '../../layout/DefaultLayout';
+import { ContentContainer, ContentHeader } from '../../layout/DefaultLayout';
+import { AlphabetMenu } from '../../layout/AlphabetMenu';
+import { MiniLoader } from '../../components/Loader';
+import { PaginationMenu } from '../../layout/PaginationMenu';
+import { useState, useEffect } from 'react';
+import { Link, useSearchParams } from 'react-router-dom';
+import api from '../../configs/api';
+
+
 
 const ArtistsIndex: React.FC = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [letter, setLetter] = useState<string>(searchParams.get('letter') || '');
+  const [pageNum, setPageNum] = useState<number>(1);
+  const [artists, setArtists] = useState<Artist[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [meta, setMeta] = useState<any>({});
+  const [responseError, setResponseError] = useState<string>('');
+
+  function handlePageChange(pageNum: number) {
+    setLoading(true);
+    setPageNum(pageNum);
+  }
+
+  function handleLetterChange(letter: string) {
+    setLoading(true);
+    setLetter(letter);
+    setPageNum(1);
+  }
+
+  useEffect(() => {
+    api.get('/artists', {
+      params: { page: pageNum, category: null, delicate: false, letter: letter }})
+      .then((response) => {
+        setArtists(response.data.artists);
+        setMeta(response.data.meta);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setLoading(false);
+        setResponseError(error.message);
+      });
+  },[pageNum, letter])
+
+
   return (
     <DefaultLayout>
-      Artists go here
+      <ContentHeader>
+        <span>::Artists</span>
+      </ContentHeader>
+      <ContentContainer>
+      {
+        loading ? <MiniLoader /> : (
+          <>
+            <AlphabetMenu activeLetter={letter} collection="artists" handleLetterChange={handleLetterChange} />
+            <div id="artists-list" className="pt-10">
+              {artists.map((artist) => (
+                <div className="entry">
+                  <Link to={`/artists/${artist.id}`}>{artist.name}</Link>
+                </div>
+              ))}
+            </div>
+          </>
+        )
+      }
+      </ContentContainer>
+      {
+        !loading &&
+          <PaginationMenu
+            pageNum={pageNum}
+            totalPages={meta.totalPages}
+            handlePageChange={handlePageChange}
+            size={12} />
+      }
     </DefaultLayout>
   );
 };
 
 export default ArtistsIndex;
+
