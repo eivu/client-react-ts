88654a5c65021bc908cbdfd26b83fcf006c14079
Rob Jenkins <384403+dabobert@users.noreply.github.com>
2024-10-19 21:43:14 -0400

    MetadatumEntry is pulling data from api

--- src/components/MetadatumEntry.tsx
+++ src/components/MetadatumEntry.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import { useAppContext } from '../store/AppContext';
 import api from '../configs/api';
-import { CiSquarePlus, CiSquareMinus } from "react-icons/ci";
+import { CiSquarePlus, CiSquareMinus, CiSquareInfo } from "react-icons/ci";
 import { Metadatum } from '../types/metadatum';
 
 
@@ -13,26 +13,46 @@ export type MetadataEntryProps = {
 }
 
 export const MetadatumEntry: React.FC<MetadataEntryProps> = ({metadatum}) => {
-  const [loading, setLoading] = useState<boolean>(true);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [dataLoaded, setDataLoaded] = useState<boolean>(false);
   const [expanded, setExpanded] = useState<boolean>(false);
+  const [files, setFiles] = useState<QueueItem[]>([]);
   const [responseError, setResponseError] = useState<String | undefined>(undefined);
-  const [queueItems, setQueueItems] = useState<QueueItem[]>([]);
   const { dispatch, queue } = useAppContext();
 
   function handleClick():void {
+    // setExpanded(!expanded);
+    if (!dataLoaded) {
+      setLoading(true);
+      api.get(`/metadata/${metadatum.id}/cloud_files`)
+        .then((response) => {
+          setFiles(response.data.cloudFiles);
+          console.log("data", response.data);
+          console.log("files", files);
+          setLoading(false);
+          setExpanded(true);
+          setDataLoaded(true);
+        })
+        .catch((error) => {
+          setLoading(false);
+          setResponseError(error.message);
+        });
+    }
   }
 
   return (
     <div>
-      { expanded ? 
-          <CiSquareMinus
-            size={32}
-            className='cursor-pointer pr-2 expander'
-            onClick={() => handleClick()}/> :
-          <CiSquarePlus
-            size={32}
-            className='cursor-pointer pr-2 expander'
-            onClick={() => handleClick()}/>
+      { loading ? 
+        <CiSquareInfo size={32} className='cursor-wait pr-2 expander' />
+        : expanded ? 
+            <CiSquareMinus
+              size={32}
+              className='cursor-pointer pr-2 expander'
+              onClick={() => handleClick()}/> :
+              <CiSquarePlus
+                size={32}
+                className='cursor-pointer pr-2 expander'
+                onClick={() => handleClick()}/>      
       }
         <Link to={`/metadata/${metadatum.id}`}>
           <span className="type">{metadatum.type}</span>
@@ -41,6 +61,15 @@ export const MetadatumEntry: React.FC<MetadataEntryProps> = ({metadatum}) => {
         <span className="pl-2">
           ({ metadatum.cloudFilesCount })
         </span>
+      <div>
+        { files?.length > 0 && //expanded &&
+          files.map((file) => (
+            <div className="entry" key={`metadatum-${metadatum}-item-entry-${file.id}`}>
+              <Link to={`/files/${file.md5}`}>{file.name}</Link>
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 };
