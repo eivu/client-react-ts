2c9e83aae836dcf99344124df2f2d7c44b47d64e
Rob Jenkins <384403+dabobert@users.noreply.github.com>
2024-10-20 13:11:13 -0400

    started work on metadata/show page

--- src/pages/Metadata/show.tsx
+++ src/pages/Metadata/show.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
-import DefaultLayout from '../../layout/DefaultLayout';
+import { FC, useEffect, useState } from 'react';
+import { Link, useLoaderData } from 'react-router-dom';
+import DefaultLayout, { ContentHeader, ContentContainer } from '../../layout/DefaultLayout';
+import api from '../../configs/api';
+import { Metadatum } from '../../types/metadatum';
 
-const MetadatumPage: React.FC = () => {
+const MetadatumPage: FC = () => {
+  const medatatumId = useLoaderData();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [metadatum, setMetadatum] = useState<Metadatum>();
+  const [responseError, setResponseError] = useState<string>('');
+
+  useEffect(() => {
+    api.get(`/metadata/${medatatumId}/cloud_files`, {
+      params: { category: null, delicate: false }}
+    ).then((response) => {
+      console.log(response.data)
+      setMetadatum(response.data.metadatum);
+      setLoading(false);
+    }).catch((error) => {
+      setLoading(false);
+      setResponseError(error.message);
+      console.log(responseError)
+    })
+  },[])
   return (
     <DefaultLayout>
       Trash goes here
