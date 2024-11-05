e9f87882c5856ad34882cf0538ff10a8c7c014f6
Robert Jenkins <384403+dabobert@users.noreply.github.com>
2024-11-05 00:02:09 -0100

    FilesTable is now extracted into its own component

---
+++ src/components/FilesTable.tsx
@@ -0,0 +1,185 @@
+import { FC, useMemo } from 'react';
+import { Link } from 'react-router-dom';
+import { FaSort, FaSortUp, FaSortDown } from "react-icons/fa";
+import type { QueueItem } from '../types/queueItem';
+import { MiniLoader } from './Loader';
+import prettyBytes from 'pretty-bytes';
+import { timeAgo } from '../common/timeAgo';
+import AVButton from './AVButton';
+import AddToQueueButton from './AddToQueueButton';
+import { FileIcon } from './FileIcon';
+
+
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  getSortedRowModel,
+  SortingState,
+  useReactTable,
+} from '@tanstack/react-table'
+
+type FilesTableProps = {
+  queueItems: QueueItem[];
+  loading: boolean;
+  responseError: string | undefined;
+  searchTerm: string;
+  sorting: SortingState;
+  setSorting: (sorting: SortingState) => void;
+}
+
+
+export const FilesTable: FC<FilesTableProps> = ({ queueItems, loading, responseError, sorting, setSorting, searchTerm }) => {
+  const columns = useMemo<ColumnDef<QueueItem>[]>(
+    () => [
+      {
+        header: () => null,
+        id: 'play',
+        enableSorting: false,
+        cell: info => (
+          <span>
+            <AVButton item={info.row.original} />
+            {info.row.original?.contentType?.startsWith('audio') ? <AddToQueueButton item={info.row.original} /> : null}
+          </span>
+        )
+      },
+      {
+        header: 'Name',
+        accessorKey: 'name',
+        cell: info => (
+          <span>
+            <div className="icon"><FileIcon contentType={info.row.original.contentType} /></div>
+            <Link to={`/files/${info.row.original.md5}`}>{info?.getValue()}</Link>
+          </span>
+        )
+      },
+      {
+        header: 'Size',
+        accessorKey: 'filesize',
+        cell: info => <span>{info?.getValue() && prettyBytes(info?.getValue())}</span>
+      },
+      {
+        header: 'Rating',
+        accessorKey: 'rating',
+      },
+      {
+        header: '# Plays',
+        accessorKey: 'numPlays',
+      },
+      {
+        header: 'Last Viewed',
+        accessorKey: 'lastViewedAt',
+        cell: info => <span>{timeAgo(info?.getValue())}</span>
+      },
+      {
+        header: 'Uploaded',
+        accessorKey: 'uploadedAt',
+        cell: info => <span>{timeAgo(info?.getValue())}</span>
+      },
+    ],
+    []
+  )
+
+  const table = useReactTable({
+    data: queueItems,
+    columns,
+    state: {
+      sorting,
+    },
+    manualSorting: true,
+    onSortingChange: setSorting,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    debugTable: true,
+    manualPagination: true, //turn off client-side pagination
+  // rowCount: dataQuery.data?.rowCount, //pass in the total row count so the table knows how many pages there are (pageCount calculated internally if not provided)
+    // pageCount: meta.totalPages,
+  })
+
+
+  return (
+    <table id="files-table" className="datatable-table border-collapse overflow-hidden break-words px-4 md:overflow-auto md:px-8">
+      <thead >
+        {table.getHeaderGroups().map(headerGroup => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map(header => {
+              return (
+                <th key={header.id} id={`${header.id}Header`}> 
+                  {header.isPlaceholder ? null : (
+                    <div
+                      {...{
+                        className: header.column.getCanSort()
+                          ? 'cursor-pointer select-none'
+                          : '',
+                        onClick: header.column.getToggleSortingHandler(),
+                      }}
+                    >
+                      {flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                      {{
+                        asc: <FaSortUp className='inline' />,
+                        desc: <FaSortDown className='inline' />,
+                        false: <FaSort  className='inline' />
+                      }[header.column.getIsSorted() as string] ?? null}
+                    </div>
+                  )}
+                </th>
+              )
+            })}
+          </tr>
+        ))}
+      </thead>
+      <tbody>
+        {
+          responseError &&
+          <tr>
+            <td colSpan={columns.length}>
+              <div className="flex items-center justify-center">
+                {responseError}
+              </div>
+            </td>
+          </tr>
+        }
+        {
+          !loading && !!searchTerm && queueItems.length === 0 &&
+          <tr>
+            <td colSpan={columns.length}>
+              <div className="flex items-center justify-center">
+                No matching files found.  Please try another search term.
+              </div>
+            </td>
+          </tr>
+        }
+        {
+          loading &&
+          <tr>
+            <td colSpan={columns.length}>
+              <MiniLoader />
+            </td>
+          </tr>
+        }
+        { !loading && table
+          .getRowModel()
+          .rows.slice(0, 100)
+          .map(row => {
+            return (
+              <tr key={row.id}>
+                {row.getVisibleCells().map(cell => {
+                  return (
+                    <td key={cell.id} className={`${cell.column.id}Col`}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </td>
+                  )
+                })}
+              </tr>
+            )
+          })}
+      </tbody>
+    </table>
+  );
+}
--- src/pages/Files/index.tsx
+++ src/pages/Files/index.tsx
@@ -1,27 +1,14 @@
-import { Link, useSearchParams } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 import { AlphabetMenu } from '../../layout/AlphabetMenu';
 import DefaultLayout, { ContentContainer, ContentHeader} from '../../layout/DefaultLayout';
-import { FaSort, FaSortUp, FaSortDown } from "react-icons/fa";
 import { PaginationMenu } from '../../layout/PaginationMenu';
-import { FileIcon } from '../../components/FileIcon';
 import  { useAppContext } from '../../store/AppContext';
 import api from '../../configs/api';
-import prettyBytes from 'pretty-bytes';
-import { timeAgo } from '../../common/timeAgo';
-import { useMemo, useState, useEffect, FC } from 'react';
-import AddToQueueButton from '../../components/AddToQueueButton';
-import AVButton from '../../components/AVButton';
+import { useState, useEffect, FC } from 'react';
 import type { QueueItem } from '../../types/queueItem';
-import { MiniLoader } from '../../components/Loader';
+import { FilesTable } from '../../components/FilesTable';
 
-import {
-  ColumnDef,
-  flexRender,
-  getCoreRowModel,
-  getSortedRowModel,
-  SortingState,
-  useReactTable,
-} from '@tanstack/react-table'
+
 
 
 
@@ -35,55 +22,7 @@ const FilesIndex: FC = () => {
   const [meta, setMeta] = useState<any>({});
   const [queueItems, setQueueItems] = useState<QueueItem[]>([]);
   const [searchTerm, setSearchTerm] = useState<string>(searchParams.get('s') || '');
-  const columns = useMemo<ColumnDef<QueueItem>[]>(
-    () => [
-      {
-        header: () => null,
-        id: 'play',
-        enableSorting: false,
-        cell: info => (
-          <span>
-            <AVButton item={info.row.original} />
-            {info.row.original?.contentType?.startsWith('audio') ? <AddToQueueButton item={info.row.original} /> : null}
-          </span>
-        )
-      },
-      {
-        header: 'Name',
-        accessorKey: 'name',
-        cell: info => (
-          <span>
-            <div className="icon"><FileIcon contentType={info.row.original.contentType} /></div>
-            <Link to={`/files/${info.row.original.md5}`}>{info?.getValue()}</Link>
-          </span>
-        )
-      },
-      {
-        header: 'Size',
-        accessorKey: 'filesize',
-        cell: info => <span>{info?.getValue() && prettyBytes(info?.getValue())}</span>
-      },
-      {
-        header: 'Rating',
-        accessorKey: 'rating',
-      },
-      {
-        header: '# Plays',
-        accessorKey: 'numPlays',
-      },
-      {
-        header: 'Last Viewed',
-        accessorKey: 'lastViewedAt',
-        cell: info => <span>{timeAgo(info?.getValue())}</span>
-      },
-      {
-        header: 'Uploaded',
-        accessorKey: 'uploadedAt',
-        cell: info => <span>{timeAgo(info?.getValue())}</span>
-      },
-    ],
-    []
-  )
+
   const { activeCategory } = useAppContext();
   const constructParams = (sorting: SortingState) => {
     return {
@@ -118,21 +57,7 @@ const FilesIndex: FC = () => {
       });
   },[sorting, pageNum, letter, searchTerm, activeCategory])
 
-  const table = useReactTable({
-    data: queueItems,
-    columns,
-    state: {
-      sorting,
-    },
-    manualSorting: true,
-    onSortingChange: setSorting,
-    getCoreRowModel: getCoreRowModel(),
-    getSortedRowModel: getSortedRowModel(),
-    debugTable: true,
-    manualPagination: true, //turn off client-side pagination
-  // rowCount: dataQuery.data?.rowCount, //pass in the total row count so the table knows how many pages there are (pageCount calculated internally if not provided)
-    // pageCount: meta.totalPages,
-  })
+
 
   function handlePageChange(pageNum: number) {
     setLoading(true);
@@ -197,89 +122,8 @@ const FilesIndex: FC = () => {
             <p className="pl-2 text-black dark:text-white">Entries Per Page</p>
           </div> */}
         </div>
-        <table id="files-table" className="datatable-table border-collapse overflow-hidden break-words px-4 md:overflow-auto md:px-8">
-          <thead >
-            {table.getHeaderGroups().map(headerGroup => (
-              <tr key={headerGroup.id}>
-                {headerGroup.headers.map(header => {
-                  return (
-                    <th key={header.id} id={`${header.id}Header`}> 
-                      {header.isPlaceholder ? null : (
-                        <div
-                          {...{
-                            className: header.column.getCanSort()
-                              ? 'cursor-pointer select-none'
-                              : '',
-                            onClick: header.column.getToggleSortingHandler(),
-                          }}
-                        >
-                          {flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
-                          {{
-                            asc: <FaSortUp className='inline' />,
-                            desc: <FaSortDown className='inline' />,
-                            false: <FaSort  className='inline' />
-                          }[header.column.getIsSorted() as string] ?? null}
-                        </div>
-                      )}
-                    </th>
-                  )
-                })}
-              </tr>
-            ))}
-          </thead>
-          <tbody>
-            {
-              responseError &&
-              <tr>
-                <td colSpan={columns.length}>
-                  <div className="flex items-center justify-center">
-                    {responseError}
-                  </div>
-                </td>
-              </tr>
-            }
-            {
-              !loading && !!searchTerm && queueItems.length === 0 &&
-              <tr>
-                <td colSpan={columns.length}>
-                  <div className="flex items-center justify-center">
-                    No matching files found.  Please try another search term.
-                  </div>
-                </td>
-              </tr>
-            }
-            {
-              loading &&
-              <tr>
-                <td colSpan={columns.length}>
-                  <MiniLoader />
-                </td>
-              </tr>
-            }
-            { !loading && table
-              .getRowModel()
-              .rows.slice(0, 100)
-              .map(row => {
-                return (
-                  <tr key={row.id}>
-                    {row.getVisibleCells().map(cell => {
-                      return (
-                        <td key={cell.id} className={`${cell.column.id}Col`}>
-                          {flexRender(
-                            cell.column.columnDef.cell,
-                            cell.getContext()
-                          )}
-                        </td>
-                      )
-                    })}
-                  </tr>
-                )
-              })}
-          </tbody>
-        </table>
+
+      <FilesTable queueItems={queueItems} loading={loading} responseError={responseError} sorting={sorting} setSorting={setSorting} searchTerm={searchTerm}/>
 
       {
         !loading &&
