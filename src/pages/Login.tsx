3c5874faffbe0569b65cbf8642e02fd9f6afbc09
Robert Jenkins <384403+dabobert@users.noreply.github.com>
2024-11-14 18:56:29 -0500

    progress with form submission - alert error is only visible on error - handlesubmit is wired to form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,15 +3,26 @@ import { useState, FC, FormEvent } from 'react';
 import { MdOutlineMailOutline, MdLockOutline } from "react-icons/md";
 import { AlertError } from '../components/AlertError';
 import DefaultLayout, { ContentHeader, ContentContainer } from '../layout/DefaultLayout';
-
+import api from '../configs/api';
 
 export const Login: FC = () => {
-  const [error, setError] = useState<string | null>(null);
+  const [error, setError] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
 
 
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
     console.log('Form submitted');
+    api.post('/api/frontend/sessions',
+      { email, password }
+    ).then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+      setError('Invalid email or password');
+    });
   }
 
   return (
@@ -20,8 +31,7 @@ export const Login: FC = () => {
         ::Login
       </ContentHeader>
       <ContentContainer>
-        {/* {error && <AlertError message={error} />} */}
-        { <AlertError message={error} /> }
+        { error && <AlertError message={error} />}
         <div className="no-scrollbar overflow-y-auto pb-20">
           <div className="w-full border-stroke dark:border-strokedark xl:w-1/2 xl:border-l-2">
             <div className="w-full p-4 sm:p-12.5 xl:p-17.5">
@@ -29,7 +39,7 @@ export const Login: FC = () => {
                 Welcome
               </h2>
 
-              <form>
+              <form onSubmit={handleSubmit}>
                 <div className="mb-4">
                   <label className="mb-2.5 block font-medium text-black dark:text-white">
                     Email
@@ -39,8 +49,8 @@ export const Login: FC = () => {
                       type="email"
                       placeholder="Enter your email"
                       className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                    />
-
+                      onChange={(e) => setEmail(e.target.value)}
+                   />
                     <span className="absolute right-4 top-4">
                       <MdOutlineMailOutline size="22" className="login-field-icon" />
                     </span>
@@ -56,6 +66,7 @@ export const Login: FC = () => {
                       type="password"
                       placeholder="Enter your password"
                       className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                      onChange={(e) => setPassword(e.target.value)}
                     />
 
                     <span className="absolute right-4 top-4">
@@ -74,7 +85,7 @@ export const Login: FC = () => {
 
                 <div className="mt-6 text-center">
                   <p>
-                    Don’t have any account?{' '}
+                    Don't have any account?{' '}
                     <Link to="/auth/signup" className="text-primary">
                       Sign Up
                     </Link>
