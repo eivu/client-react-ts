86a2af64323da850297398898e36036275956715
Robert Jenkins <384403+dabobert@users.noreply.github.com>
2024-11-14 18:01:04 -0500

    app is now sending the JWT from env var

--- src/configs/api.ts
+++ src/configs/api.ts
@@ -4,12 +4,11 @@ import axios from 'axios';
 
 
 
-
 const api = axios.create({
   baseURL: import.meta.env.VITE_EIVU_SERVER_HOST + '/api/frontend/v1/',
   headers: {
     "Content-Type": "application/json",
-    'Authorization': 'Bearer ' + import.meta.env.VITE_EIVU_USER_TOKEN
+    'Authorization': 'Bearer ' + import.meta.env.VITE_EIVU_JWT
   },
   params: {
     keyFormat: 'camel_lower',
