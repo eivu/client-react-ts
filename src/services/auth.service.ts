ab557e7e2b73f5ffd033b19e26f253f4d6b6ab7c
Robert Jenkins <384403+dabobert@users.noreply.github.com>
2024-11-16 13:55:44 -0500

    created auth.service

---
+++ src/services/auth.service.ts
@@ -0,0 +1,19 @@
+import api from './api';
+
+
+export const login = (email: string, password: string) => {
+  return api
+    .post('sessions',
+      { email, password }
+    ).then((response) => {
+      console.log(response);
+      // if (response.data.accessToken) {
+      //   localStorage.setItem("user", JSON.stringify(response.data));
+      // }
+      return response
+    })
+    .catch((error) => {
+      console.log(error);
+      return error
+    });
+}
