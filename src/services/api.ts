c1d367fefb3c6619fdaaad96a9a13bb8c235a85b
Robert Jenkins <384403+dabobert@users.noreply.github.com>
2024-11-16 14:51:15 -0500

    clientUuid is now sent along with every request

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,14 @@
 import axios from 'axios';
+import { v4 as uuid } from 'uuid'
+
+const clientUuid = () => {
+  if (!localStorage.getItem('clientUuid')) {
+    localStorage.setItem('clientUuid', uuid());
+  }
+  return localStorage.getItem('clientUuid');
+}
+
+localStorage.getItem('clientUuid') || uuid();
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_EIVU_SERVER_HOST + '/api/frontend/v1/',
@@ -8,7 +18,8 @@ const api = axios.create({
   },
   params: {
     keyFormat: 'camel_lower',
-    delicate: false
+    delicate: false,
+    clientUuid: clientUuid()
   }
 });
 
