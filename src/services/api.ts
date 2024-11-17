46904b264741db999ee2cfbc1d2e43bfc76bb70e
Robert Jenkins <384403+dabobert@users.noreply.github.com>
2024-11-16 20:14:05 -0500

    after login user object is stored in local storage with its token - added jwt-decode to package - created getCurrentUser in auth.service - created user type - services/api now uses token from user local storage

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { v4 as uuid } from 'uuid'
+import { getCurrentUser } from './auth.service';
 
 const clientUuid = () => {
   if (!localStorage.getItem('clientUuid')) {
@@ -14,7 +15,7 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_EIVU_SERVER_HOST + '/api/frontend/v1/',
   headers: {
     "Content-Type": "application/json",
-    'Authorization': 'Bearer ' + import.meta.env.VITE_EIVU_JWT
+    'Authorization': 'Bearer ' + getCurrentUser()?.token
   },
   params: {
     keyFormat: 'camel_lower',
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,15 +1,29 @@
 import api from './api';
-
+import { jwtDecode } from "jwt-decode";
+import { User } from '../types/user';
 
 export const login = (email: string, password: string) => {
+  let user: User;
   return api
     .post('sessions',
       { email, password }
     ).then((response) => {
-      console.log(response);
-      // if (response.data.accessToken) {
-      //   localStorage.setItem("user", JSON.stringify(response.data));
-      // }
+      if (response.data.data.user) {
+        user = {...jwtDecode(response.data.data.user), token: response.data.data.user};
+        localStorage.setItem("user", JSON.stringify(user));
+      }
       return response
     });
+}
+
+export const getCurrentUser = () => {
+  const userStr: string | null = localStorage.getItem('user');
+
+  try {
+    if (userStr) return JSON.parse(userStr);
+  } catch (error) {
+    return null;
+  }
+
+  return null;
 }
