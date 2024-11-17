ef8b007f371be39d71852572c696517e3f5a4315
Robert Jenkins <384403+dabobert@users.noreply.github.com>
2024-11-16 23:16:02 -0500

    services/api is now reading token directly from token local storage - it now has its own key to get around cannot access before initialization error

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { v4 as uuid } from 'uuid'
 import { getCurrentUser } from './auth.service';
+import { User } from '../types/user';
 
 const clientUuid = () => {
   if (!localStorage.getItem('clientUuid')) {
@@ -9,13 +10,11 @@ const clientUuid = () => {
   return localStorage.getItem('clientUuid');
 }
 
-localStorage.getItem('clientUuid') || uuid();
-
 const api = axios.create({
   baseURL: import.meta.env.VITE_EIVU_SERVER_HOST + '/api/frontend/v1/',
   headers: {
     "Content-Type": "application/json",
-    'Authorization': 'Bearer ' + getCurrentUser()?.token
+    'Authorization': 'Bearer ' + localStorage.getItem("token")
   },
   params: {
     keyFormat: 'camel_lower',
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -11,17 +11,19 @@ export const login = (email: string, password: string) => {
       if (response.data.data.user) {
         user = {...jwtDecode(response.data.data.user), token: response.data.data.user};
         localStorage.setItem("user", JSON.stringify(user));
+        localStorage.setItem("token", user.token);
       }
       return response
     });
 }
 
 export const logout = () => {
-  localStorage.removeItem('user');
+  localStorage.removeItem("user");
+  localStorage.removeItem("token");
 }
 
 export const getCurrentUser = () => {
-  const userStr: string | null = localStorage.getItem('user');
+  const userStr: string | null = localStorage.getItem("user");
 
   try {
     if (userStr) return JSON.parse(userStr);
