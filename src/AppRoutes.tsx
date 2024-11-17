be1bf4d9c2a7db310f8ce6aab71e6955e38774f1
Robert Jenkins <384403+dabobert@users.noreply.github.com>
2024-11-17 11:08:17 -0500

    protected routes are working

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -17,7 +17,7 @@ import { Login } from './pages/Login';
 import { Home } from './pages/Home';
 import { About } from './pages/About';
 
-const ProtectedRoute = () => {
+const ProtectedRoutes = () => {
   const user = getCurrentUser();
 
   if (!user) return <Navigate to="/login" />;
@@ -31,7 +31,7 @@ const router = createBrowserRouter([
         <PageTitle title="EIVU" />
         <Home />
       </>,
-    path: "/home",
+    path: "/",
   },
   {
     element: 
@@ -41,121 +41,126 @@ const router = createBrowserRouter([
       </>,
     path: "/about",
   },
-  {
-    element: 
-      <>
-        <PageTitle title="EIVU" />
-        <FilesIndex valid_files={true} />
-      </>,
-    path: "/",
-  },
-  {
-    element: 
-      <>
-        <PageTitle title="EIVU::Files" />
-        <FilesIndex valid_files={true}/>
-      </>,
-    path: "/files"
-  },
-  {
-    element: <File />,
-    path: "/files/:fileId",
-    loader: ({ params }) => {
-      return params.fileId;
-    },
-  },
-  {
-    element: 
-      <>
-        <PageTitle title="EIVU::Queue" />
-        <Queue />
-      </>,
-    path: "/queue"
-  },
-  {
-    element: 
-      <>
-        <PageTitle title="EIVU::Artists" />
-        <ArtistsIndex />
-      </>,
-    path: "/artists"
-  },
-  {
-    element: <ArtistPage />,
-    path: "/artists/:artistId",
-    loader: ({ params }) => {
-      return params.artistId;
-    },
-  },
-  {
-    element: 
-      <>
-        <PageTitle title="EIVU::Releases" />
-        <ReleasesIndex />
-      </>,
-    path: "/releases"
-  },
-  {
-    element: 
-      <>
-        <PageTitle title="EIVU::Releases" />
-        <ReleasePage />
-      </>,
-    path: "/releases/:releaseId",
-    loader: ({ params }) => {
-      return params.releaseId;
-    },
-  },
-  {
-    element: 
-      <>
-        <PageTitle title="EIVU::Folders" />
-        <FoldersIndex />
-      </>,
-    path: "/folders"
-  },
-  {
-    element: 
-      <>
-        <PageTitle title="EIVU::Metadata" />
-        <MetadataIndex />
-      </>,
-    path: "/metadata"
-  },
-  {
-    element: 
-      <>
-        <PageTitle title="EIVU::Metadatum" />
-        <MetadatumPage />
-      </>,
-    path: "/metadata/:metadatumId",
-    loader: ({ params }) => {
-      return params.metadatumId;
-    },
-  },
-  {
-    element: 
-      <>
-        <PageTitle title="EIVU::Trash" />
-        <FilesIndex valid_files={false} />
-      </>,
-    path: "/trash"
-  },
-  {
-    element: 
-      <>
-        <PageTitle title="EIVU::Auth" />
-        <AuthPage />
-      </>,
-    path: "/auth"
-  },
-  {
+    {
     element: 
       <>
         <PageTitle title="EIVU::Auth" />
         <Login />
       </>,
     path: "/login"
+  },
+  {
+    element: <ProtectedRoutes />,
+    children: [
+      {
+          element: 
+            <>
+              <PageTitle title="EIVU" />
+              <FilesIndex valid_files={true} />
+            </>,
+          path: "/",
+        },
+        {
+          element: 
+            <>
+              <PageTitle title="EIVU::Files" />
+              <FilesIndex valid_files={true}/>
+            </>,
+          path: "/files"
+        },
+        {
+          element: <File />,
+          path: "/files/:fileId",
+          loader: ({ params }) => {
+            return params.fileId;
+          },
+        },
+        {
+          element: 
+            <>
+              <PageTitle title="EIVU::Queue" />
+              <Queue />
+            </>,
+          path: "/queue"
+        },
+        {
+          element: 
+            <>
+              <PageTitle title="EIVU::Artists" />
+              <ArtistsIndex />
+            </>,
+          path: "/artists"
+        },
+        {
+          element: <ArtistPage />,
+          path: "/artists/:artistId",
+          loader: ({ params }) => {
+            return params.artistId;
+          },
+        },
+        {
+          element: 
+            <>
+              <PageTitle title="EIVU::Releases" />
+              <ReleasesIndex />
+            </>,
+          path: "/releases"
+        },
+        {
+          element: 
+            <>
+              <PageTitle title="EIVU::Releases" />
+              <ReleasePage />
+            </>,
+          path: "/releases/:releaseId",
+          loader: ({ params }) => {
+            return params.releaseId;
+          },
+        },
+        {
+          element: 
+            <>
+              <PageTitle title="EIVU::Folders" />
+              <FoldersIndex />
+            </>,
+          path: "/folders"
+        },
+        {
+          element: 
+            <>
+              <PageTitle title="EIVU::Metadata" />
+              <MetadataIndex />
+            </>,
+          path: "/metadata"
+        },
+        {
+          element: 
+            <>
+              <PageTitle title="EIVU::Metadatum" />
+              <MetadatumPage />
+            </>,
+          path: "/metadata/:metadatumId",
+          loader: ({ params }) => {
+            return params.metadatumId;
+          },
+        },
+        {
+          element: 
+            <>
+              <PageTitle title="EIVU::Trash" />
+              <FilesIndex valid_files={false} />
+            </>,
+          path: "/trash"
+        },
+        {
+          element: 
+            <>
+              <PageTitle title="EIVU::Auth" />
+              <AuthPage />
+            </>,
+          path: "/auth"
+        },
+    ]
   }
 ]);
 
