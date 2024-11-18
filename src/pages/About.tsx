4462a5558626165b29791b0771c2e39b6a099b6f
Rob Jenkins <384403+dabobert@users.noreply.github.com>
2024-11-18 13:41:20 -0500

    fixed about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -15,38 +15,39 @@ const faqItems: Faq[] = [
     title: 'Why eivu?',
     details:
       'Why not.',
-  },{
-    title: 'No really, why eivu?',
-    details:
-      'In this modern media and tech landscape of ours you no longer blown your own content, you merely pay for access to it.  However if the distribution rights or politics change you can <a href="https://www.avclub.com/disney-scrubbed-trans-rights-episode-of-moon-girl-and-devil-dinosaur">lose access to it</a>. Plus you like I may love Spotify but it sucks to a). Or have access to all of the mp3s you "aquired" over the years and all the playlists you painstakingly curated in iTunes (yes Apple Music users we all know how cool you are). Well eivu is the answer to that',
-  },{
-    title: 'Why not Plex?',
-    details:
-      'Plex is amazing, but it requires an up front cost of money and time into infrastructure.  Plex is also advantageous if you plan to have multiple 10s of terabytes. The Plex of a dear friend is around 90 TB.  If however you mainly want access to your own collection of mp3s, Plex may be overkill',
-  },{
-    title: 'Do you recommend using AWS S3?',
-    details:
-      'Not really. AWS optimizes all of its services to work best and most cost effectively with itself.  AWS bandwidth costs is free if you send the content to other AWS services. When working outside the network, though AWS is cheap for storage, you pay through the nose for bandwidth. When I first sarted using eivu, my costs of bandwidth would be 10x what I was paying for storage. As such I would recommend wasabi.  It costs 6 per TB. And bandwidth is free as long as you "stream" less than you store.  Ie if you have 2tb of content, you pay nothing as long as you don\'t stream all of it',
-  },{
-    title: 'What do you get out of this?',
-    details:
-      'Access to my own content. And knowledge. I plan reimplement eivu, server and client, into any forthcoming language I want to learn. Now that stuff is already working and architected I can just figure out how to do it again in rust or nestjs',
-  },{
-    title: 'Your sign up functionality Is broken, how can I sign up?',
-    details:
-      'Again...... who are you?',
-  },{
-    title: '',
-    details:
-    '',
-  },{
-    title: '',
-    details:
-    },{
-    title: '',
-    details:
-      'It takes 2-3 weeks to get your first blog post ready. That includes the in-depth research & creation of your monthly content ui/ux strategy that we your first blog post.',
-  },
+  }
+  // ,{
+  //   title: 'No really, why eivu?',
+  //   details:
+  //     'In this modern media and tech landscape of ours you no longer blown your own content, you merely pay for access to it.  However if the distribution rights or politics change you can <a href="https://www.avclub.com/disney-scrubbed-trans-rights-episode-of-moon-girl-and-devil-dinosaur">lose access to it</a>. Plus you like I may love Spotify but it sucks to a). Or have access to all of the mp3s you "aquired" over the years and all the playlists you painstakingly curated in iTunes (yes Apple Music users we all know how cool you are). Well eivu is the answer to that',
+  // },{
+  //   title: 'Why not Plex?',
+  //   details:
+  //     'Plex is amazing, but it requires an up front cost of money and time into infrastructure.  Plex is also advantageous if you plan to have multiple 10s of terabytes. The Plex of a dear friend is around 90 TB.  If however you mainly want access to your own collection of mp3s, Plex may be overkill',
+  // },{
+  //   title: 'Do you recommend using AWS S3?',
+  //   details:
+  //     'Not really. AWS optimizes all of its services to work best and most cost effectively with itself.  AWS bandwidth costs is free if you send the content to other AWS services. When working outside the network, though AWS is cheap for storage, you pay through the nose for bandwidth. When I first sarted using eivu, my costs of bandwidth would be 10x what I was paying for storage. As such I would recommend wasabi.  It costs 6 per TB. And bandwidth is free as long as you "stream" less than you store.  Ie if you have 2tb of content, you pay nothing as long as you don\'t stream all of it',
+  // },{
+  //   title: 'What do you get out of this?',
+  //   details:
+  //     'Access to my own content. And knowledge. I plan reimplement eivu, server and client, into any forthcoming language I want to learn. Now that stuff is already working and architected I can just figure out how to do it again in rust or nestjs',
+  // },{
+  //   title: 'Your sign up functionality Is broken, how can I sign up?',
+  //   details:
+  //     'Again...... who are you?',
+  // },{
+  //   title: '',
+  //   details:
+  //   '',
+  // },{
+  //   title: '',
+  //   details:
+  //   },{
+  //   title: '',
+  //   details:
+  //     'It takes 2-3 weeks to get your first blog post ready. That includes the in-depth research & creation of your monthly content ui/ux strategy that we your first blog post.',
+  // },
 ];
 
 
