1b53a0d14a204003683c3d5160cf3d0bcb2ba32c
Rob Jenkins <384403+dabobert@users.noreply.github.com>
2024-11-23 09:48:41 -0500

    moving header styles out of component into css

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,9 +9,9 @@ const Header = (props: {
 }) => {
   const user = getCurrentUser();
   return (
-    <header className="top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
+    <header id="top-nav" className="">
       <div className="flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11">
-        <div className="flex items-center gap-2 sm:gap-4 lg:hidden">
+        <div className="flex items-center gap-2 lg:hidden">
           {/* <!-- Hamburger Toggle BTN --> */}
           <button
             aria-controls="sidebar"
