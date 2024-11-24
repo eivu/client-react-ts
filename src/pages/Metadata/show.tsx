5344caeb076bf5c79db50868199a2e539f47595b
Robert Jenkins <384403+dabobert@users.noreply.github.com>
2024-11-24 01:14:36 -0500

    progress with metadata show using files table

--- src/pages/Metadata/show.tsx
+++ src/pages/Metadata/show.tsx
@@ -1,47 +1,53 @@
-import { FC, useEffect, useState } from 'react';
-import { Link, useLoaderData, useSearchParams } from 'react-router-dom';
-import DefaultLayout, { ContentHeader, ContentContainer } from '../../layout/DefaultLayout';
-import api from '../../services/api.config';
-import { Metadatum } from '../../types/metadatum';
-import { MiniLoader } from '../../components/Loader';
-import prettyBytes from 'pretty-bytes';
-import { timeAgo } from '../../common/timeAgo';
-import { QueueItem } from '../../types/queueItem';
+import { useSearchParams, useLoaderData, Link } from 'react-router-dom';
+import { AlphabetMenu } from '../../layout/AlphabetMenu';
+import DefaultLayout, { ContentContainer, ContentHeader} from '../../layout/DefaultLayout';
 import { PaginationMenu } from '../../layout/PaginationMenu';
-import { useAppContext } from '../../store/AppContext';
+import  { useAppContext } from '../../store/AppContext';
+import api from '../../services/api.config';
+import { useState, useEffect, FC } from 'react';
+import type { QueueItem } from '../../types/queueItem';
+import { FilesTable } from '../../components/FilesTable';
+import { cleanseSearchParams } from '../../common/cleanseSearchParams';
 import { ErrorPanel } from '../../components/ErrorPanel';
+
 
 const MetadatumPage: FC = () => {
   const medatatumId = useLoaderData();
+  const [metadatum, setMetadatum] = useState<Metadata>();
   const [searchParams, setSearchParams] = useSearchParams();
+  const [searchSubmittedAt, setSearchSubmittedAt] = useState<number>(Date.now());
   const [loading, setLoading] = useState<boolean>(true);
-  const [metadatum, setMetadatum] = useState<Metadatum>();
-  const [responseError, setResponseError] = useState<string>('');
-  const [files, setFiles] = useState<QueueItem[]>([]);
-  const [meta, setMeta] = useState<any>({});
-  const { activeCategory } = useAppContext();
+  const [sorting, setSorting] = useState<SortingState>([{
+    id: searchParams.get('sortBy') || 'name',
+    desc: searchParams.get('sortDesc') || false
+  }])
+  const [responseError, setResponseError] = useState<string | undefined>(undefined);
+  const [letter, setLetter] = useState<string>(searchParams.get('letter') || '');
   const [pageNum, setPageNum] = useState<number>(Number(searchParams.get('pageNum')) || 1);
-
-  function handlePageChange(pageNum: number) {
-    setLoading(true);
-    setPageNum(pageNum);
-    setSearchParams({ pageNum: pageNum.toString() });
+  const [meta, setMeta] = useState<any>({});
+  const [queueItems, setQueueItems] = useState<QueueItem[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>(searchParams.get('s') || '');
+  const { activeCategory } = useAppContext();
+  const constructParams = (sorting: SortingState) => {
+    return {
+      category: activeCategory,
+      delicate: false,
+      sortBy: sorting[0]?.id,
+      page: pageNum,
+      sortDesc: sorting[0]?.desc,
+      letter: letter,
+      search_term: searchTerm,
+    }
   }
 
-
-  useEffect(() => {
-    setPageNum(1);
-    setSearchParams({ pageNum: 1 });
-    // setSearchParams({ pageNum: 1, letter: letter, s: searchTerm });
-  },[activeCategory])
-
   useEffect(() => {
     setLoading(true);
+    setSearchParams(cleanseSearchParams(searchParams));
     api.get(`/metadata/${medatatumId}`, {
       params: { page: pageNum, category: activeCategory, delicate: false }}
     ).then((response) => {
       setMetadatum(response.data.metadatum);
-      setFiles(response.data.files);
+      setQueueItems(response.data.files);
       setMeta(response.data.meta);
       setLoading(false);
     }).catch((error) => {
@@ -49,58 +55,90 @@ const MetadatumPage: FC = () => {
       setResponseError(error.message);
       console.log(responseError)
     })
-  },[pageNum, activeCategory])
+  },[sorting, pageNum, letter, searchSubmittedAt, activeCategory])
+
+  function handlePageChange(pageNum: number) {
+    setLoading(true);
+    setPageNum(pageNum);
+    searchParams.set('pageNum', pageNum.toString());
+    searchParams.set('letter', letter);
+    searchParams.set('sortBy', sorting()[0].id);
+    searchParams.set('sortDesc', sorting()[0].desc);
+    setSearchParams(searchParams);
+  }
+
+  function handleSearchKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      setLoading(true);
+      setPageNum(1);
+      setLetter('');
+      setSearchSubmittedAt(Date.now());
+      setSearchParams({ pageNum: 1, letter: '', s: event.target.value });
+      setSearchTerm(event.target.value);
+    }
+  }
+
+  function handleLetterChange(letter: string) {
+    event?.preventDefault();
+    setLoading(true);
+    setLetter(letter);
+    setPageNum(1);
+    searchParams.set('pageNum', '1');
+    searchParams.set('letter', letter);
+    searchParams.set('sortBy', sorting()[0].id);
+    searchParams.set('sortDesc', sorting()[0].desc);
+    setSearchParams(searchParams);
+  }
+
+  function handleSortChange(sorting: SortingState) {
+    setSorting(sorting);
+    searchParams.set('pageNum', '1');
+    searchParams.set('sortBy', sorting()[0].id);
+    searchParams.set('sortDesc', sorting()[0].desc);
+    setSearchParams(searchParams);
+  }
+
   return (
     <DefaultLayout>
-      {/* PageTitle: ::{artist?.secured ? `Artist ${artist?.id}` : artist?.name} */}
-      { metadatum &&
+      { !loading &&
           <ContentHeader>::
-            <Link to="/metadata" className="breadcrumb">Metadata</Link>::[{metadatum.type}]{metadatum.value}
+            <Link to="/metadata" className="breadcrumb">Metadata</Link>::[{metadatum.type}]::{
+              responseError ? 'Err0r' :
+                metadatum?.secured ? `Metadatum ${metadatum?.id}` : metadatum?.value
+            }
           </ContentHeader>
       }
-      {
-        loading ?
-          <MiniLoader /> : (
-            <ContentContainer>
-              {
-                responseError ? <ErrorPanel errorMessage={responseError} /> :
-                  metadatum?.filesCount === 0 ?
-                    <div className="text-center text-gray-500">No cloud files found</div> :
-                      <table>
-                        <thead>
-                          <tr>
-                            <th></th>
-                            <th>Name</th>
-                            <th>Size</th>
-                            <th>Rating</th>
-                            <th># Plays</th>
-                            <th>Last Viewed</th>
-                            <th>Uploaded At</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          {
-                          files.map((file) => {
-                            return (
-                              <tr key={`metadatum-file-entry-${file.md5}`}>
-                                <td></td>
-                                <td><Link to={`/files/${file.md5}`}>{file.name}</Link></td>
-                                <td>{prettyBytes(file.filesize)}</td>
-                                <td>{file.rating}</td>
-                                <td>{file.numPlays}</td>
-                                <td>{file.lastViewedAt && timeAgo(file.lastViewedAt)}</td>
-                                <td>{timeAgo(file.uploadedAt)}</td>
-                              </tr>
-                            )
-                          })
-                          }
-                        </tbody>
-                      </table>
-              }
-            </ContentContainer>
-              
-          )
-      }
+
+      <section id="content-container" className="data-table-common data-table-two rounded-sm border border-stroke bg-white py-4 shadow-default dark:border-strokedark  dark:bg-boxdark">
+      <div>
+        <AlphabetMenu activeLetter={letter} collection="files" handleLetterChange={handleLetterChange} />
+        {/* <div className="flex justify-between border-b border-stroke px-8 pb-4 dark:border-strokedark">
+          {/* Search Field * /}
+          <div className="w-100">
+            <input
+              id="search"
+                type="text"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                className="w-full rounded-md border border-stroke px-5 py-2.5 outline-none focus:border-primary dark:border-strokedark dark:bg-meta-4 dark:focus:border-primary"
+                placeholder="Search..."
+                onKeyDown={handleSearchKeyDown}
+              />
+          </div>
+        </div> */}
+
+
+        </div>
+        {
+          responseError ? <ErrorPanel errorMessage={responseError} /> :
+        <FilesTable
+        queueItems={queueItems}
+        loading={loading}
+        responseError={responseError}
+        sorting={sorting}
+        searchTerm={searchTerm}
+        setSorting={handleSortChange} /> }
       {
         !loading && !responseError &&
           <PaginationMenu
@@ -109,8 +147,10 @@ const MetadatumPage: FC = () => {
             handlePageChange={handlePageChange}
             size={12} />
       }
+      </section>
     </DefaultLayout>
   );
 };
 
 export default MetadatumPage;
+1
