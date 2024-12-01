35d98db3248b7d5371c8305a3d08301d5a82466b
Robert Jenkins <384403+dabobert@users.noreply.github.com>
2024-11-30 23:36:54 -0500

    got a rough version of the video tracker working

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,6 +1,8 @@
+import { useState } from 'react';
 import { VideoLayout } from './Player/layouts/video-layout';
 import { defaultLayoutIcons } from '@vidstack/react/player/layouts/default';
 import { QueueItem } from "../types/queueItem";
+import api from '../services/api.config';
 import {
   MediaPlayer,
   MediaProvider,
@@ -18,8 +20,48 @@ export type VideoPlayerProps = {
 }
 
 
-export function VideoPlayer({item, player}:VideoPlayerProps):ReactElement {
+export const VideoPlayer:ReactElement = ({item, player}:VideoPlayerProps) => {
+  // const [trackTimer, setTrackTimer] = useState<number>(0);
 
+  // const [unmarkedTrack, setUnmarkedTrack] = useState<boolean>(true);
+  const MIN_PLAYING_DURATION:number =  1000;
+  let trackTimer:any = undefined;
+  let unmarkedTrack:boolean = true;
+
+  function setTimer():void {
+    // console.log('tracking', item.name);
+    // let x = setTimeout(updateServerStats, MIN_PLAYING_DURATION)
+    // only set timer if track is not marked as played.
+    if (unmarkedTrack) {
+      trackTimer = setTimeout(updateServerStats, MIN_PLAYING_DURATION);
+    }
+      // const x:number = setTimeout(updateServerStats, MIN_PLAYING_DURATION)
+      // setTrackTimer(x);
+    // );
+  }
+
+  function onSeeked():void {
+    clearTimeout(trackTimer);
+    setTimer();
+  }
+
+  function updateServerStats():void {
+    if (unmarkedTrack) {
+      console.log('updateServerStats: item', item.name);
+
+      // make sure Whispers of Silence is never tracked
+      // currentTrack?.md5 != "D258C1A40E785406564616AFD8045351" &&
+      api.post(`/cloud_files/${item.md5}/update_stats`)
+        .then((response) => {
+          console.log('track stats updated', response);
+        }).catch((error) => {
+          console.log('error occured while trying to update track stats', error);
+        })
+
+      clearTimeout(trackTimer);
+      unmarkedTrack = false;
+    }
+  }
 
 
   // We can listen for the `can-play` event to be notified when the player is ready.
@@ -36,6 +78,8 @@ export function VideoPlayer({item, player}:VideoPlayerProps):ReactElement {
       crossOrigin
       playsInline
       onCanPlay={onCanPlay}
+      onPlay={setTimer}
+      onSeeked={onSeeked}
       ref={player}
     >
       <MediaProvider>
