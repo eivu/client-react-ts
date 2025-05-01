de37af9dd24bcd59d5aeffdd6f9d46ba6117d4d5
Robert Jenkins <384403+dabobert@users.noreply.github.com>
2025-05-01 00:43:24 -0400

    progress with offline file reading

--- src/pages/OfflinePlayer.tsx
+++ src/pages/OfflinePlayer.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from 'react'
 import { Link } from 'react-router-dom';
 import DefaultLayout, { ContentHeader, ContentContainer } from '@src/layout/DefaultLayout';
-
+import FileDropZone from '@src/components/FileDropZone';
 
 export const OfflinePlayer: React.FC = () => {
   return (
@@ -11,7 +11,8 @@ export const OfflinePlayer: React.FC = () => {
       </ContentHeader>
 
       <ContentContainer>
-        <iframe src="/ejs.html" style={{ width: '100%', height: '100vh', border: 'none' }}></iframe>
+        {/* <iframe src="/ejs.html" style={{ width: '100%', height: '100vh', border: 'none' }}></iframe> */}
+        <FileDropZone />
       </ContentContainer>
     </DefaultLayout>
   );
