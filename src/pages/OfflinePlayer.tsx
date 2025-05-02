0c9812899468cdf6fa8d5781ff327a14d3d98689
Robert Jenkins <384403+dabobert@users.noreply.github.com>
2025-05-02 16:47:25 -0400

    working with format tests

--- src/pages/OfflinePlayer.tsx
+++ src/pages/OfflinePlayer.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import DefaultLayout, { ContentHeader, ContentContainer } from '@src/layout/DefaultLayout';
 import FileDropZone from '@src/components/FileDropZone';
+import FormatsTest from '@src/components/FormatsTest';
 
 export const OfflinePlayer: React.FC = () => {
   return (
@@ -11,6 +12,7 @@ export const OfflinePlayer: React.FC = () => {
       </ContentHeader>
 
       <ContentContainer>
+        <FormatsTest />
         {/* <iframe src="/ejs.html" style={{ width: '100%', height: '100vh', border: 'none' }}></iframe> */}
         <FileDropZone />
       </ContentContainer>
