c755f39959d39ade0612c76d60206931f3a4b52f
Robert Jenkins <384403+dabobert@users.noreply.github.com>
2025-05-02 20:53:06 -0400

    slight refactor

--- src/components/ArcadePlayer/EmulatorJsPlayer.tsx
+++ src/components/ArcadePlayer/EmulatorJsPlayer.tsx
@@ -4,9 +4,8 @@ import { ROM_FORMATS } from './index';
 import { ES_CONTROLLER_MAPPINGS } from './ejs_controller_mappings';
 
 export const EmulatorJsPlayer: React.FC<ViewerProps> = ({ file }) => {
-  const core = ROM_FORMATS[file.contentType].core || 'unkown';
+  const core = ROM_FORMATS[file.contentType].core || 'unknown';
   const controllerMapping = ES_CONTROLLER_MAPPINGS["Google Stadia"];
   const gameMapping = controllerMapping[core] || controllerMapping.unknown;
-
-  return <iframe id="emulatorjs" src={`/emulatorjs.html?name=${file.name}&rom=${file.url}&mapping=${JSON.stringify(gameMapping)}&core=${core}`}></iframe>;
+  return <iframe id="emulatorjs" src={`/emulatorjs.html?name=${file.name}&md5=${file.md5}&rom=${file.url}&mapping=${JSON.stringify(gameMapping)}&core=${core}`}></iframe>;
 };
